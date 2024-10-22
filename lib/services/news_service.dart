9441ab84360664335d3cc8cf1a3cbe829a8a8dae
AhmedAbdelfattah703 <ahmed.abdelfattah703@gmail.com>
2024-10-23 00:53:21 +0300

    test commint

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -9,9 +9,9 @@ class NewsService{
   final response = await dio.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=e5dbe77693904649940819a5305d777a');
   print(response);
 }
-void getSportsNews() async {
+// void getSportsNews() async {
   
-  final response = await dio.get('https://newsapi.org/v2/everything?q=sports&apiKey=e5dbe77693904649940819a5305d777a');
-  print(response);
-}
+//   final response = await dio.get('https://newsapi.org/v2/everything?q=sports&apiKey=e5dbe77693904649940819a5305d777a');
+//   print(response);
+// }
 }
