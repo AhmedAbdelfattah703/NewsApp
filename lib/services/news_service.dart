14e526ae39528707e1f46cc567b2e091126a4f41
AhmedAbdelfattah703 <ahmed.abdelfattah703@gmail.com>
2024-10-24 15:19:17 +0300

    test 3

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,14 +1,25 @@
 import 'package:dio/dio.dart';
+import 'package:news_app_ui_setup/models/article_model.dart';
 
-class NewsService{
+class NewsService {
   final Dio dio;
 
   NewsService(this.dio);
-  
-  getNews()async{
-    Response responce =await dio.get('https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=e5dbe77693904649940819a5305d777a#');
-  
-  Map<String,dynamic> jsonData = responce.data;
-  print(jsonData);
+
+ Future<List<ArticleModel>> getNews() async {
+    Response responce = await dio.get(
+        'https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=e5dbe77693904649940819a5305d777a#');
+
+    Map<String, dynamic> jsonData = responce.data;
+    List<dynamic> articles = jsonData['articles'];
+    List<ArticleModel> articlesList = [];
+    for (var article in articles) {
+      ArticleModel articleModel = ArticleModel(
+          image: article['urlToImage'],
+          title: article['title'],
+          subTitle: article['description']);
+          articlesList.add(articleModel);
+    }
+   return articlesList; 
   }
 }
