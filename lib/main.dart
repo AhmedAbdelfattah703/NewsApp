4267bf3117a0743dc2f802f198f2e221f685571d
AhmedAbdelfattah703 <ahmed.abdelfattah703@gmail.com>
2024-10-24 11:01:06 +0300

    Test 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/services/news_service.dart';
 import 'package:news_app_ui_setup/views/home_view.dart';
 //import 'package:news_app/widgets/category_card.dart';
 // import 'package:news_app_ui_setup/main.dart';
 void main() {
-  getNews();
+  NewsService(Dio()).getNews();
   runApp(const NewsApp());
-}
-final dio = Dio();
-void getNews() async {
-  
-  final response = await dio.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=e5dbe77693904649940819a5305d777a');
-  print(response);
 }
 
 class NewsApp extends StatelessWidget {
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -3,15 +3,12 @@ import 'package:dio/dio.dart';
 class NewsService{
   final Dio dio;
 
-  NewsService({required this.dio});
-  void getGenaralNews() async {
+  NewsService(this.dio);
   
-  final response = await dio.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=e5dbe77693904649940819a5305d777a');
-  print(response);
-}
-// void getSportsNews() async {
+  getNews()async{
+    Response responce =await dio.get('https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=e5dbe77693904649940819a5305d777a#');
   
-//   final response = await dio.get('https://newsapi.org/v2/everything?q=sports&apiKey=e5dbe77693904649940819a5305d777a');
-//   print(response);
-// }
+  Map<String,dynamic> jsonData = responce.data;
+  print(jsonData);
+  }
 }
